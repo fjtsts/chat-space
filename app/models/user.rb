adf08f3603a9a7f10b33a2f0fc0daba9262b5b98
satoshi fujita <xxxx3104@yahoo.co.jp>
2019-05-10 18:03:57 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  # has_many :groups, through: members
+ 
   has_many :messages
-  has_many :members 
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
