98b688591db4ce07425fdab5597ae4ac82b52568
satoshi fujita <xxxx3104@yahoo.co.jp>
2019-05-14 21:54:10 +0900

    UpadteAsynchronous

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,33 @@
 $(function(){
   function buildHTML(message){
-    var imagehtml = message.image == null?"" : <img src="${message.image}"class="lower-message_image"></img>
+    var addImage = '';
+    if (message.image.url) {
+      addImage = `<img src="${message.image.url}">`;
+    }
     var html = `<div class="message">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
-                      ${messages.users.name}
+                      ${message.user_name}
                     </div>
                     <div class="upper-message__date">
-                      ${messages.created_at}
+                      ${message.created_at}
                     </div>
                   </div>
                   <div class="lower-meesage">
                     <p class="lower-message__content">
-                      ${messages.content}
+                      ${message.content}
                     </p>
-                      ${messages.image}
+                      ${addImage}
                   </div>
                 </div>`
     return html;
   }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
+    console.log(url)
     $.ajax({
       url: url,
       type: "POST",
@@ -34,8 +39,9 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html)
-      $('.form__message').reset()
-      $('.messages').animate({scrollTop: $('messages')[0].scrollHeight}, 'fast');
+      $('#new_message')[0].reset();
+      $('.form__submit').prop( 'disabled', false );
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
       alert('error');
