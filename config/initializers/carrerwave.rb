97fdad6f57c8c644de2a22b92445d2d441d5af82
satoshi fujita <xxxx3104@yahoo.co.jp>
2019-05-22 23:45:03 +0900

    UpdateS3

--- config/initializers/carrerwave.rb
+++ config/initializers/carrerwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'fjtsts'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bucket'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fjtsts'
 end
